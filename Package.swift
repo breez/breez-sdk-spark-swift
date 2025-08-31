06c4c5909bb537d787342989e374081149f2cf25
SDK release tagger <no-reply@breez.technology>
2025-08-31 18:59:52 +0000

    Update Swift bindings to version 0.1.0-rc8

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .library(name: "BreezSdkSpark", targets: ["breez_sdk_sparkFFI", "BreezSdkSpark"]),
     ],
     targets: [
-        .binaryTarget(name: "breez_sdk_sparkFFI", url: "https://github.com/breez/breez-sdk-spark-swift/releases/download/0.1.0-rc7/breez_sdk_sparkFFI.xcframework.zip", checksum: "0cca309d657852c3a78a100e8e655ae0531588a55929580b34281590b5b7dcf1"),
+        .binaryTarget(name: "breez_sdk_sparkFFI", url: "https://github.com/breez/breez-sdk-spark-swift/releases/download/0.1.0-rc8/breez_sdk_sparkFFI.xcframework.zip", checksum: "b3e2caceedd680bbd75a8ad65cbcb2c60611abd8ecacff02e74cc67f9d9c2aea"),
         .target(name: "BreezSdkSpark", dependencies: ["breez_sdk_sparkFFI"]),
     ]
 )
