0af016bddc6e6f5101b7dd4490705432c7279c68
SDK release tagger <no-reply@breez.technology>
2026-01-28 17:52:47 +0000

    Update Swift bindings to version 0.7.13

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/mkrd/Swift-BigInt.git", from: "2.0.0")
     ],
     targets: [
-        .binaryTarget(name: "breez_sdk_sparkFFI", url: "https://github.com/breez/breez-sdk-spark-swift/releases/download/0.7.13/breez_sdk_sparkFFI.xcframework.zip", checksum: "180ee36e836383bb592a10433a3b2d5915f20923b6a163ade16da13d45db4871"),
+        .binaryTarget(name: "breez_sdk_sparkFFI", url: "https://github.com/breez/breez-sdk-spark-swift/releases/download/0.7.13/breez_sdk_sparkFFI.xcframework.zip", checksum: "8d8ae1e39ac9cf608761a604a025e931e3dfebfefc46464cce6f15c7219a33d6"),
         .target(
             name: "BreezSdkSpark",
             dependencies: [
