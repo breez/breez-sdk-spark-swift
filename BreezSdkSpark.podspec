919d5debb8d6d0624c46aba83974033058306947
SDK release tagger <no-reply@breez.technology>
2026-02-11 07:54:56 +0000

    Update Swift bindings to version 0.7.19

--- BreezSdkSpark.podspec
+++ BreezSdkSpark.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name                   = "BreezSdkSpark"
-  spec.version                = "0.7.18"
+  spec.version                = "0.7.19"
   spec.license                = { :type => "MIT" }
   spec.summary                = "Swift bindings to the Breez Spark SDK"
   spec.homepage               = "https://breez.technology"
--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/mkrd/Swift-BigInt.git", from: "2.0.0")
     ],
     targets: [
-        .binaryTarget(name: "breez_sdk_sparkFFI", url: "https://github.com/breez/breez-sdk-spark-swift/releases/download/0.7.18/breez_sdk_sparkFFI.xcframework.zip", checksum: "36a473dec9108c74729b9240e45c84161bddf0c378790f91b9bcdc194c2b89b3"),
+        .binaryTarget(name: "breez_sdk_sparkFFI", url: "https://github.com/breez/breez-sdk-spark-swift/releases/download/0.7.19/breez_sdk_sparkFFI.xcframework.zip", checksum: "982ae0cddf43bd9d79fa6346369fbd7cda7f67ae380d490d82cd1d8656f10478"),
         .target(
             name: "BreezSdkSpark",
             dependencies: [
